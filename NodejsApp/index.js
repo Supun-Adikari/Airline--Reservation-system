5553569f2b9a52135f7c88b0a1900c2a0b1b31ab
Usitha Jayaweera <119028577+UsithaDJay@users.noreply.github.com>
2023-01-07 19:56:55 +0530

    Node.js App added
    Frontend and backend combined

---
+++ NodejsApp/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+const dotenv = require('dotenv');
+
+var apiController = require("./API/API");
+var authController = require("./API/Auth");
+
+var {RestoreSession} = require("./MODEL/Authentication");
+
+var app = express();
+dotenv.config();
+
+RestoreSession();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+
+//Set up EJS//udj
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "DELETE, PUT");
+    next();
+});
+//udj
+app.use('/', apiController)
+
+app.use('/api',apiController);
+app.use('/auth',authController);
+
+app.listen(process.env.port);
